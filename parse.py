04849d3b089624c04926926bc5fbd72f8ee9a2d2
NguyenAndrew <anguyen29@wisc.edu>
2018-04-06 14:03:26 -0500

    Fix all errors associated with running in Windows

--- parse.py
+++ parse.py
@@ -1,7 +1,8 @@
 import csv
 from sys import argv
+
 script,JudgesNumber = argv
-with open('submissions.csv', 'r') as f:
+with open('submissions.csv', 'r', encoding="utf8") as f:
     reader = csv.reader(f)
     l = list(reader)
 
@@ -13,12 +14,12 @@ for row in l:
      for x in range(1,11):
          del row[3]
          
-with open('/Users/apple/Desktop/output.csv', 'wb') as csv_file:
+with open('output.csv', 'w', newline='') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
     writer.writerow(l[0]+['Table Number']+['Assigned Judges'])
     l.remove(l[0])
-    print len(l)
+    print(len(l))
     a=1
     for line in l:
-        writer.writerow(line+[a]+[(a-1)/((len(l)/int(JudgesNumber)))+1])
+        writer.writerow(line+[a]+[int(((a-1)/int((len(l)/int(JudgesNumber)))+1))])
         a+=1
